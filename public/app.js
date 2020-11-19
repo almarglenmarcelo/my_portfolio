3cc7bdfc8243c140b7d09800b45854b86ff8021e
agmar28 <ag.marcelo28@gmail.com>
2020-11-20 00:26:54 +0800

    Init Final Revision

--- public/app.js
+++ public/app.js
@@ -1,1 +1,137 @@
-console.log("Client-Sided JS"),$(document).ready(function(){const e=document.querySelector(".splash-screen");setTimeout(()=>{e.classList.add("display-none")},2500),document.querySelectorAll('a[href^="#"]').forEach(e=>{e.addEventListener("click",function(e){e.preventDefault(),document.querySelector(this.getAttribute("href")).scrollIntoView({behavior:"smooth"})})}),$(".accordion i").click(()=>{$(".navbar").toggleClass("active"),$(".accordion i").toggleClass("active")}),$("a").click(()=>{$(".navbar").removeClass("active"),$(".accordion i").removeClass("active")}),$(window).scroll(function(){0===this.scrollY&&($(".nav").addClass("extra"),setTimeout(()=>{$(".nav").removeClass("extra")},300)),this.scrollY>20?($(".nav").addClass("landingpage"),this.scrollY>600?$(".nav").addClass("aboutme"):$(".nav").removeClass("aboutme"),this.scrollY>1220?$(".nav").addClass("whatdefinesme"):$(".nav").removeClass("whatdefinesme"),this.scrollY>2100?$(".nav").addClass("projectinfo"):$(".nav").removeClass("projectinfo"),this.scrollY>2500?$(".nav").addClass("contactinfo"):$(".nav").removeClass("contactinfo")):($(".nav").removeClass("landingpage"),$(".nav").removeClass("aboutme"),$(".nav").removeClass("whatdefinesme"),$(".nav").removeClass("projectinfo"),$(".nav").removeClass("contactinfo"),$(".about").removeClass("fade-in"))}),setTimeout(()=>{new Typed("#text",{strings:["  A third-year BSIT undergraduate.","  An aspiring Developer. ","  Dreaming to be a Full-Stack Developer","  Eager to learn anything.","  A cup of coffee helps. ","  Frustrations can't stop me from learning.","  Improvement is a must. ","  No pain, No gain. "],typeSpeed:80,backSpeed:40,loop:!0})},4200)});
+console.log('Client-Sided JS')
+
+$(document).ready(function () {
+
+    //Splash Screen
+    const splash = document.querySelector('.splash-screen');
+    setTimeout(() => {
+        splash.classList.add('display-none');
+    }, 2500)
+
+    //For smooth anchor link tag animation
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // //Change background-color animation
+    // $('.mainprofile').hover(() => {
+    //     $('.mainprofile').parent().parent().css({
+    //         "background-color": "#bdc667",
+    //         "color": "#2b2d42"
+    //     })
+    //     $('.mainprofile').parent().siblings('#greetings').css({
+    //         "color": "#2b2d42"
+    //     })
+    //     $('.mainprofile').parent().siblings('#description').children().css({
+    //         "-webkit-text-stroke": "#2b2d42 2px"
+    //     })
+    //     $('.mainprofile').parent().siblings('.icons').children().children().children().css({
+    //         "color": "#2b2d42"
+    //     })
+    // }, () => {
+    //     $('.mainprofile').parent().parent().css({
+    //         "background-color": "#2b2d42",
+    //         "color": "#fff"
+    //     })
+    //     $('.mainprofile').parent().siblings('#greetings').css({
+    //         "color": "lightgrey"
+    //     })
+    //     $('.mainprofile').parent().siblings('#description').children().css({
+    //         "-webkit-text-stroke": "#bdc667"
+    //     })
+    //     $('.mainprofile').parent().siblings('.icons').children().children().children().css({
+    //         "color": "#fff"
+    //     })
+    // })
+  
+
+    //Toggle Accordion Menu
+    $('.accordion i').click(() => {
+        $('.navbar').toggleClass('active');
+        $('.accordion i').toggleClass('active')
+    })
+
+    $('a').click(() => {
+        $('.navbar').removeClass('active');
+        $('.accordion i').removeClass('active')
+    })
+
+    //Change navbar color when scrolling
+    $(window).scroll(function () {
+        // sticky navbar on scroll script
+        if (this.scrollY === 0) {
+            $('.nav').addClass('extra')
+            setTimeout(() => {
+                $('.nav').removeClass('extra');
+            }, 300)
+        }
+        if (this.scrollY > 20) {
+            $('.nav').addClass("landingpage");
+
+            if (this.scrollY > 600) {
+                $('.nav').addClass('aboutme')
+
+            } else {
+                $('.nav').removeClass('aboutme');
+
+            }
+
+            if (this.scrollY > 1520) {
+                $('.nav').addClass('whatdefinesme');
+
+            } else {
+                $('.nav').removeClass('whatdefinesme');
+
+            }
+
+            if (this.scrollY > 2300) {
+                $('.nav').addClass('projectinfo');
+            } else {
+                $('.nav').removeClass('projectinfo');
+            }
+
+
+            if (this.scrollY > 2900) {
+                $('.nav').addClass('contactinfo');
+            } else {
+                $('.nav').removeClass('contactinfo');
+            }
+
+        } else {
+            $('.nav').removeClass("landingpage");
+            $('.nav').removeClass('aboutme');
+            $('.nav').removeClass('whatdefinesme');
+            $('.nav').removeClass('projectinfo');
+            $('.nav').removeClass('contactinfo');
+            $('.about').removeClass('fade-in');
+        }
+
+    });
+
+    // typing text animation script
+
+    setTimeout(() => {
+        var typed = new Typed("#text", {
+            strings: ['  A third-year BSIT undergraduate.',
+                '  An aspiring Developer. ',
+                '  Dreaming to be a Full-Stack Developer',
+                '  Eager to learn anything.',
+                '  A cup of coffee helps. ',
+                '  Frustrations can\'t stop me from learning.',
+                '  Improvement is a must. ',
+                '  No pain, No gain. '
+            ],
+            typeSpeed: 80,
+            backSpeed: 40,
+            loop: true
+        });
+    }, 4200);
+
+});
+
